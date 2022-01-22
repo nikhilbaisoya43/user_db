c4297e9a70660c7b657f77f7bf58ed58331fa88b
Nikhil Baisoya <76607126+nikhilbaisoya43@users.noreply.github.com>
2022-01-22 14:59:40 +0530

    Update User.go

--- User.go
+++ User.go
@@ -1,7 +1,10 @@
 package main
 
 import "gorm.io/gorm"
-
+ /*
+ *User is used as medium to store and perfrom operation in db
+ *user give json format data and decoding to the database
+ */
 type User struct {
 	gorm.Model
 	Name string `json:"name"`
